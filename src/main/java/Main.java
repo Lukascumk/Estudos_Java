b9fb1de98dfa6d8c95edb3bfee09e5ba93902b3d
LukascomK <lukas_motta@outlook.com>
2024-02-27 18:17:05 +0000

    2
    Declarando variáveis e chamando variáveis e imprimindo.
    Fazendo alguns cálculos para entender como funciona a linguagem Java.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,31 @@
 
 public class Main {
   public static void main(String[] args) {
+
+
+  int number1 =5, number2 = 15, number3 = 20;
+
+
+  int resultado = number1 + (number2 * number3);
+
+      
+  int num = 50;
+  String nome = "LukascomK";
+    
+    
     System.out.println("Hello world!");
     System.out.print("Lukas\n");
-    System.out.printf("%s - %d", "Lukas", 2024);
-    System.out.printf("Nome:%s%nAno:%d", "Lukas", 2024);
+    System.out.printf("%s - %d\n" , "Lukas", 2024);
+    System.out.printf("Nome:%s%nAno:%d%n", "Lukas",2024);
 
+    
+System.out.printf("Valor da variável é : %d\nNome: %s\n", num, nome);
+
+    
+System.out.printf( "Os valores escondigos nos numeros 1, 2, 3 são %d %d %d  \n", number1, number2, number3); 
+
+    
+System.out.printf("A soma dos valores são : %d", resultado);
     
   }
 
