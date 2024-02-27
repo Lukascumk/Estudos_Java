a5630b12a060239b794dbdd922257bd20d1de170
LukascomK <lukas_motta@outlook.com>
2024-02-27 11:38:42 +0000

    Comandos de impressão em tela
    Fiz os primeiros comandos de impressão utilizando printf, println e somente o print.
    
    O println efetua quebra de linha após a mensagem gravada.
    O print pra efetuar a quebra de linha você pode colocar barra invertida no final da mensagem, juntamente com as aspas para efetuar o mesmo conceito do println.
    
    Printf para impressão de  para imprimir variáveis como int, string, float e etc.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,11 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
+    System.out.print("Lukas\n");
+    System.out.printf("%s - %d", "Lukas", 2024);
+    System.out.printf("Nome:%s%nAno:%d", "Lukas", 2024);
+
+    
   }
 
  
